7efa722bf300cb3827d8a4097c37bf036720932a
SummCoder <2404174187@qq.com>
2024-01-13 01:08:48 +0800

    实现注册与登录，以及token进行身份认知

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from flask import Flask, request, jsonify, session
+from datetime import timedelta
+from flask import Flask, request, jsonify
+from flask_jwt_extended import create_access_token, jwt_required, JWTManager, get_jwt_identity
 from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 import os
@@ -6,23 +8,32 @@ import sys
 import click
 
 app = Flask(__name__)
-app.secret_key = 'miniChat'
+
 WIN = sys.platform.startswith('win')
 if WIN:  # 如果是 Windows 系统，使用三个斜线
     prefix = 'sqlite:///'
 else:  # 否则使用四个斜线
     prefix = 'sqlite:////'
 
-app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = prefix + os.path.join(app.root_path, 'data.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # 关闭对模型修改的监控
+app.config['JWT_SECRET_KEY'] = 'minichat'
+app.config['JWT_ACCESS_TOKEN_EXPIRES'] = timedelta(hours=72)
 # 在扩展类实例化前加载配置
 db = SQLAlchemy(app)
+jwt = JWTManager(app)
 
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20))
+    username = db.Column(db.String(20))     # 用户名
+    password_hash = db.Column(db.String(128))   # 密码散列值
+
+    def set_password(self, password):   # 用于设置密码
+        self.password_hash = generate_password_hash(password)
+
+    def validate_password(self, password):  # 用于验证密码
+        return check_password_hash(self.password_hash, password)
 
 
 @app.cli.command()  # 注册为命令，可以传入 name 参数来自定义命令
@@ -35,12 +46,6 @@ def initdb(drop):
     click.echo('Initialized database.')  # 输出提示信息
 
 
-users = {
-    'user1': 'password1',
-    'user2': 'password2'
-}
-
-
 @app.route('/')
 def welcome():
     return 'Welcome to My miniChat Project!'
@@ -51,13 +56,19 @@ def register():
     data = request.get_json()
     username = data.get('username')
     password = data.get('password')
-    if username in users:
+    user = User.query.filter_by(username=username).first()
+    if user:
         return jsonify(code=999, msg='该用户名已存在')
     elif not all([username, password]):
         """应该是不会发生的，在前端对于信息是否为空进行检验"""
         return jsonify(code=400, msg='参数不完整')
     else:
-        return jsonify(code=201, msg='新用户创建成功！')
+        user_new = User(username=username)
+        user_new.set_password(password)
+        db.session.add(user_new)
+        db.session.commit()
+        access_token = create_access_token(identity=username)
+        return jsonify(code=201, data='Bearer ' + access_token, msg='新用户创建成功！')
 
 
 @app.route('/user/login', methods=['POST'])
@@ -65,29 +76,25 @@ def login():
     data = request.get_json()
     username = data.get('username')
     password = data.get('password')
-    if username in users and users[username] == password:
-        session['username'] = username
-        return jsonify(code=200, msg='登录成功！')
+    user = User.query.filter_by(username=username).first()
+    if user and user.validate_password(password):
+        access_token = create_access_token(identity=username)
+        return jsonify(code=200, data='Bearer ' + access_token, msg='登录成功！')
     else:
-        return jsonify({'message': 'Invalid username or password'})
+        return jsonify(code=999, msg='用户名或密码错误')
 
 
-@app.route("/user/logout")
-def logout():
-    # 删除 Session 中保存的用户名
-    session.pop("username", None)
+# @app.route("/user/logout")
+# def logout():
+#     return "logout"
 
 
 @app.route("/session", methods=["GET"])
+@jwt_required()
 def check_session():
-
-    """登录校验"""
-    username = session.get("username")
-    if username:
-        return jsonify(msg=username)
-    else:
-        return jsonify(msg="登录校验未通过")
+    current_user = get_jwt_identity()
+    return jsonify(current_user)
 
 
 if __name__ == '__main__':
-    app.run(debug=False)
+    app.run(debug=True)
